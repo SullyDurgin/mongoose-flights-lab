09283f1f5cbdbc5273594fe5e8ab8ac79845d2b6
SullyDurgin <katsdurgin@gmail.com>
2021-10-30 19:19:59 -0400

    add price and seat

--- models/flight.js
+++ models/flight.js
@@ -1,30 +1,40 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const flightSchema = new Schema(
+	{
+		airline: {
+			type: String,
+			required: true,
+		},
 
-const flightSchema = new Schema({
-  airline: {
-    type: String,
-    required: true
-  },
+		flightNo: {
+			type: Number,
+		},
 
-  flightNo: {
-    type: Number, 
-  },
+		airport: {
+			type: String,
+			enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+		},
+		departs: [String],
+		seatsAvail: Boolean,
+	},
+	{
+		timestamps: true,
+	}
+)
 
-  airport: {
-    type: String,
-    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"]
-  },
-  departs: [String],
-  seatsAvail: Boolean,
-}, {
-  timestamps: true
+const ticketSchema = new Schema({
+	seat: {
+		type: String,
+	},
+
+	price: {
+		type: Number,
+	},
 })
 
-const Flight = mongoose.model("Flight", flightSchema)
+const Flight = mongoose.model('Flight', flightSchema, 'Tickets', ticketSchema)
 
-export {
-  Flight,
-}
+export { Flight }
